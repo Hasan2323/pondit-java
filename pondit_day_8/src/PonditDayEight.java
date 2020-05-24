dc10e501d0d25be183a4800083cc042c3cc8372e
Saimon <ahsaimon.ctg@gmail.com>
2020-05-24 19:19:21 +0600

    modified day-8 file.

--- pondit_day_8/src/PonditDayEight.java
+++ pondit_day_8/src/PonditDayEight.java
@@ -4,7 +4,8 @@
 * 3. JDK path is stored in an EV called JAVA_HOME
 * 4. args. this just a name of array. It can be changed. You can write saimon instead of args.
 * 5. System.out.println(args[0]); it doesn't produce error in compile time but it will produce ArrayIndexOutOfBoundException in runtime.
-* to solve this you have to pass a argument while running. ex: java PonditDayEight hello or "hello".
+* to solve this you have to pass arguments while running. ex: java PonditDayEight hello or "hello". and in IDE
+* go to edit configuration. write value in Program arguments field.
 * 6. if you add/multiply two byte, it will be an integer. ex: byte +/* bye = int
 * and, short +/* short = int and int +/* int = int. here / means or.
 * so if anyone wants to add/multiply 2 byte/short type values, then he needs to write return type int or higher than int.
