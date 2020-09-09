75f5ba5a9bcda6eb50c893e0bcc7a014670a44c4
Saimon <ahsaimon.ctg@gmail.com>
2020-09-10 01:48:24 +0600

    Assignment one - Calculator - calculate unlimited time.

--- Assignment-one/src/four/multipleClassTwo/Runner.java
+++ Assignment-one/src/four/multipleClassTwo/Runner.java
@@ -10,26 +10,43 @@ import java.util.Scanner;
 public class Runner {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Type number one: ");
-        double num1 = scanner.nextDouble();
+        boolean go = true;
 
-        System.out.print("Type operator(+,-,*,/): ");
-        char operator = scanner.next().charAt(0);
+        while (go) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Type number one: ");
+            double num1 = scanner.nextDouble();
 
-        System.out.print("Type number two: ");
-        double num2 = scanner.nextDouble();
+            System.out.print("Type operator(+,-,*,/): ");
+            char operator = scanner.next().charAt(0);
 
-        if (operator == '/' && num2 < 1) {
-            System.out.println("For division, divisor must be greater than 0");
-            return;
+            System.out.print("Type number two: ");
+            double num2 = scanner.nextDouble();
+
+            if (operator == '/' && num2 < 1) {
+                System.err.println("Error: For division, divisor must be greater than 0");
+                System.out.print("\n\nWanna calculate again? Type anything for Yes and 'n' for No: ");
+                char choice = scanner.next().charAt(0);
+                if (choice == 'n') {
+                    go = false;
+                    System.out.println("Bye then! Take care.");
+                }
+                continue;
+            }
+
+            Operation operation = CalculatorFactory.getOperator(operator);
+            operation.calculate(num1, num2);
+
+            double result = operation.getResult();
+            System.out.printf("%.1f %c %.1f = %.1f %n", num1, operator, num2, result);
+
+            System.out.print("Wanna calculate again? Type anything for Yes and 'n' for No: ");
+            char choice = scanner.next().charAt(0);
+            if (choice == 'n') {
+                go = false;
+                System.out.println("Bye then! Take care.");
+            }
         }
-
-        Operation operation = CalculatorFactory.getOperator(operator);
-        operation.calculate(num1, num2);
-
-        double result = operation.getResult();
-        System.out.printf("%.1f %c %.1f = %.1f %n", num1, operator, num2, result);
 
     }
 }
