20ee77b672a72d584aad87f06881f42642d53814
Saimon <ahsaimon.ctg@gmail.com>
2021-04-28 15:14:27 +0600

    range updated

--- pondit_day_7/src/DataType.java
+++ pondit_day_7/src/DataType.java
@@ -6,13 +6,13 @@ public class DataType {
 
     public static void main(String[] args) {
 
-        byte num = 127; // [8 bit] range: -128 to 127 (-2^7 to 2^7-1). That means 2^8=256. -128 theke 127 porjonto total 256 ta.
+        byte num = 127; // [8 bit or 1 byte] range: -128 to 127 (-2^7 to 2^7 - 1). That means 2^8=256. -128 theke 127 porjonto total 256 ta.
 
-        short num1 = 32767; // [16 bit] range: -32,768 to 32,767 (-2^15 to 2^15-1)
+        short num1 = 32767; // [16 bit or 2 bytes] range: -32,768 to 32,767 (-2^15 to 2^15 -1)
 
-        int num3 = 2147483647; // [32 bit] range: -2,14,74,83,648 to 2,14,74,83,647 (-2^31 to 2^31-1)
+        int num3 = 2147483647; // [32 bit or 4 bytes] range: -2,14,74,83,648 to 2,14,74,83,647 (-2^31 to 2^31 - 1)
 
-        long num4 = 9223372036854775807L; // [64 bit] range: from -9223372036854775808 to 9223372036854775807. (-2^63 to 2^63-1)
+        long num4 = 9223372036854775807L; // [64 bit or 8 bytes] range: from -9223372036854775808 to 9223372036854775807. (-2^63 to 2^63 - 1)
         // Note that you should end the value with an "L":
 
         String str = "this is a string";
